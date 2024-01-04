f6921a8c04278a1109ce73ad8641be42869c203c
Hank Lin <tsuhan_lin@phison.com>
2024-01-04 16:58:51 +0800

    feat: combine react router

--- src/index.tsx
+++ src/index.tsx
@@ -1,47 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Root from './routes/root'
-import ErrorPage from './routes/error_page'
-import styles from './index.module.css'
-
-const App = (): JSX.Element => {
-  return <div className={styles.textColor}>Hello, world! Debug!!!!!!~~</div>
-}
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Root />,
-    children: [
-      {
-        index: true,
-        element: <App />,
-      },
-      {
-        path: '/about',
-        children: [
-          {
-            index: true,
-            element: <div>About</div>,
-          },
-          {
-            path: '/about/me',
-            element: <div>About me</div>,
-          },
-        ],
-      },
-      {
-        path: '*',
-        element: <ErrorPage />,
-      },
-    ],
-  },
-  {
-    path: '/other-entry',
-    element: <div>other-entry</div>,
-  },
-])
+import App from './App'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLInputElement
@@ -49,6 +8,6 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 )

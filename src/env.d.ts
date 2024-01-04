3cd8e94aff568f543bc26db836b7669eb764cf27
hanklin <hanklin21000@gmail.com>
2024-01-05 00:34:26 +0800

    feat: add layout

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,4 +1,14 @@
 declare module '*.module.css' {
-    const classes: Record<string, string>;
-    export default classes;
+  const classes: Record<string, string>
+  export default classes
+}
+
+declare module '*.png' {
+  const value: string
+  export default value
+}
+
+declare module '*.svg' {
+  const value: string
+  export default value
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      <h1>Home</h1>
+      <h1>
+        ref:https://github.com/TailAdmin/free-react-tailwind-admin-dashboard
+      </h1>
     </div>
   )
 }

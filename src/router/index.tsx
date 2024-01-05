753f08308cc858e9d8f914f89fcebafc6f868f4b
Hank Lin <tsuhan_lin@phison.com>
2024-01-05 21:36:22 +0800

    draft

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Layout from '../layout'
+import { DefaultLayout } from '../layouts'
 import Home from '../pages/Home'
 import About from '../pages/About'
 import ErrorPage from '../pages/ErrorPage'
@@ -8,7 +8,7 @@ import ErrorPage from '../pages/ErrorPage'
 const Router = createBrowserRouter([
   {
     path: '/',
-    element: <Layout />,
+    element: <DefaultLayout />,
     children: [
       {
         index: true,

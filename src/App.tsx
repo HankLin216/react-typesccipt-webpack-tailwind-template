fa8b026af247b98914a29ba900c0d21164a1778d
hanklin <hanklin21000@gmail.com>
2024-01-09 01:46:30 +0800

    draft

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Router from './router'
 import { RouterProvider } from 'react-router-dom'
 import './App.css'
 
 const App = (): JSX.Element => {
+  useEffect(() => {
+    // add class to body element
+    document.body.classList.add('bg-lightPrimary')
+  }, [])
+
   return <RouterProvider router={Router} />
 }
 
